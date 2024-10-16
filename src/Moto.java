dcace0cc69e23c509e8f83bf6f4425fa76391178
Bruno Kenji Ayabe <brunoayabe@edu.unifil.br>
2024-10-15 23:57:08 -0300

    Initial commit

---
+++ src/Moto.java
@@ -0,0 +1,17 @@
+package com.company;
+
+class Moto implements Veiculo {
+    @Override
+    public void acelerar() {
+        System.out.println("A moto está acelerando.");
+    }
+
+    @Override
+    public void frear() {
+        System.out.println("A moto está freando.");
+    }
+
+    public void grau() {
+        System.out.println("A moto está dando grau.");
+    }
+}
